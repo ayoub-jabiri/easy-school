16904e19b0024a1810264756c2192fcb91d58b2b
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-08-03 10:32:42 +0100

    Finished the user register test cases

--- back-end/__tests__/integrated/auth.test.js
+++ back-end/__tests__/integrated/auth.test.js
@@ -1,6 +1,6 @@
 import request from "supertest";
 import app from "../../app.js";
-import { testingToken } from "../setup.js";
+import { adminTestingToken, teacherTestingToken } from "../setup.js";
 import User from "../../modules/users/user.model.js";
 
 describe("user register", () => {
@@ -16,7 +16,7 @@ describe("user register", () => {
                     password: "12345678",
                     passwordConfirm: "12345678",
                 })
-                .set("Authorization", `Bearer ${testingToken}`);
+                .set("Authorization", `Bearer ${adminTestingToken}`);
 
             expect(res.statusCode).toEqual(201);
             expect(res.body).toHaveProperty("message");
@@ -28,6 +28,67 @@ describe("user register", () => {
             const user = await User.findOne({ email: "johndoe1@gmail.com" });
             expect(user).not.toBeNull();
             expect(user).toHaveProperty("fullName", "John Doe 1");
+        });
+    });
+
+    describe("failure test cases", () => {
+        test("user registration fails due to missing admin token", async () => {
+            const res = await request(app).post("/api/users/auth/register");
+
+            expect(res.statusCode).toEqual(401);
+            expect(res.body).toHaveProperty("message");
+        });
+
+        test("user registration fails due to not being an admin", async () => {
+            const res = await request(app)
+                .post("/api/users/auth/register")
+                .set("Authorization", `Bearer ${teacherTestingToken}`);
+
+            expect(res.statusCode).toEqual(403);
+            expect(res.body).toHaveProperty("message");
+        });
+
+        test("user registration fails due to missing required body", async () => {
+            const res = await request(app)
+                .post("/api/users/auth/register")
+                .set("Authorization", `Bearer ${adminTestingToken}`);
+
+            expect(res.statusCode).toEqual(400);
+            expect(res.body).toHaveProperty("message");
+        });
+
+        test("user registration fails due to missing required data", async () => {
+            const res = await request(app)
+                .post("/api/users/auth/register")
+                .send({
+                    fullName: "",
+                    phoneNumber: "",
+                    email: "",
+                    role: "",
+                    password: "",
+                    passwordConfirm: "",
+                })
+                .set("Authorization", `Bearer ${adminTestingToken}`);
+
+            expect(res.statusCode).toEqual(400);
+            expect(res.body).toHaveProperty("message");
+        });
+
+        test("user registration fails due to already existing email", async () => {
+            const res = await request(app)
+                .post("/api/users/auth/register")
+                .send({
+                    fullName: "John Doe 2",
+                    phoneNumber: "0612345678",
+                    email: "johndoe1@gmail.com",
+                    role: "teacher",
+                    password: "12345678",
+                    passwordConfirm: "12345678",
+                })
+                .set("Authorization", `Bearer ${adminTestingToken}`);
+
+            expect(res.statusCode).toEqual(400);
+            expect(res.body).toHaveProperty("message");
         });
     });
 });
--- back-end/__tests__/setup.js
+++ back-end/__tests__/setup.js
@@ -4,24 +4,20 @@ import User from "../modules/users/user.model.js";
 import { hashPassword, signToken } from "../utils/user.utils.js";
 
 let mongoServer;
-export let testingUser;
-export let testingToken;
+export const adminTestingToken = signToken({
+    _id: "64b8f1e2c9e77f0012345674",
+    fullName: "John Doe",
+    role: "admin",
+});
+export const teacherTestingToken = signToken({
+    _id: "64b8f1e2c9e77f0012345678",
+    email: "janesmith@gmail.com",
+    role: "teacher",
+});
 
 beforeAll(async () => {
     mongoServer = await MongoMemoryServer.create();
     await mongoose.connect(mongoServer.getUri());
-
-    const hashedPassword = await hashPassword("12345678");
-
-    testingUser = await User.create({
-        fullName: "John Doe",
-        phoneNumber: "0612345678",
-        email: "johndoe@gmail.com",
-        role: "admin",
-        password: hashedPassword,
-    });
-
-    testingToken = signToken(testingUser);
 });
 
 afterAll(async () => {
