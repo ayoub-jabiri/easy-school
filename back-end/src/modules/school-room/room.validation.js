8ce1650939c04c0198df75ea0b37d640eff01607
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-08-27 15:43:35 +0100

    Set up school-room model and schema

--- back-end/src/modules/school-room/room.validation.js
+++ back-end/src/modules/school-room/room.validation.js
@@ -4,7 +4,7 @@ export const schoolRoomSchema = z.object({
     roomNumber: z.number({
         error: (iss) =>
             iss.input == undefined
-                ? "The room number is required"
-                : "The room number must be a number",
+                ? "The schoolroom number is required"
+                : "The schoolroom number must be a number",
     }),
 });
