1d8b911e998bc665ee79c3ba78a57842ae9b4511
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-09-10 16:08:50 +0100

    Set up the front-end environment

---
+++ front-end/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <>
+            <h1 className="text-5xl not-first-of-type:font-bold">
+                Hello World!
+            </h1>
+        </>
+    );
+}
+
+export default App;
